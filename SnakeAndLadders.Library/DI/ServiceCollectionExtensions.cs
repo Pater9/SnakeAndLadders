8d9436aa203b2d76b45231fcd7bb77b32fdbc55a
Jon Paternain <jpaternain@gmail.com>
2023-01-25 19:35:53 +0100

    Add DI utility method

---
+++ SnakeAndLadders.Library/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SnakeAndLadders.Library
+{
+    public static class SnakesAndLaddersServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSnakeAndLaddersServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDiceThrower, DiceThrower>();
+            services.AddSingleton<GameManager>();
+
+            return services;
+        }
+    }
+}
